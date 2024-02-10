07fab25f511c9fa9bb016bd1eb30a44379ee6116
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-02-10 11:40:07 +0300

    Створено сід для заповнення таблиці referrals_user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersSeeder::class,
             PursesSeeder::class,
+            ReferralsUserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
