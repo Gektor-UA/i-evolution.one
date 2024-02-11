3363cd26aa9351a404580e973ef5749842f8a82c
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-02-11 08:16:50 +0300

    Скрипти тимчасово підключені на сторінких. Файл не підключається

--- resources/views/cabinet.blade.php
+++ resources/views/cabinet.blade.php
@@ -74,4 +74,27 @@
 
         </div>
     </div>
+
+
+
+
+
+    <script>
+        document.getElementById('iHealthRefLink').addEventListener('click', function() {
+            var refLink = this.getAttribute('data-ref-link');
+
+            var input = document.createElement('textarea');
+            input.value = refLink;
+            document.body.appendChild(input);
+
+            input.select();
+            var result = document.execCommand('copy');
+
+            document.body.removeChild(input);
+
+            return false;
+        });
+    </script>
 @endsection
+
+
