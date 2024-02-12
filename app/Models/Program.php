b03c362ad4b00bfda467bbca10fa93013d530fe2
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-02-12 10:38:52 +0300

    Створено міграція для таблиці з програмами

---
+++ app/Models/Program.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+
+    protected $table = 'programs';
+
+    protected $fillable = [
+        'program_name',
+        'first_amount',
+        'second_amount',
+        'third_amount',
+    ];
+}
