be73794e052bdd5659227bba5953cc742a8096b2
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-02-12 12:15:20 +0300

    Додано функціонал вибору програми

--- app/Http/Controllers/ProgramsUserController.php
+++ app/Http/Controllers/ProgramsUserController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProgramsUser;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProgramsUserController extends Controller
 {
-    //
+    /**
+     * Збереження вибраної програми користувачем.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function savePackage(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'package_id' => 'required|integer',
+        ]);
+
+        $userId = Auth::id();
+
+        ProgramsUser::create([
+            'user_id' => $userId,
+            'program_id' => (int) $validatedData['package_id'],
+        ]);
+
+        return response()->json(['message' => 'Вибрану програму збережено успішно'], 200);
+    }
 }
