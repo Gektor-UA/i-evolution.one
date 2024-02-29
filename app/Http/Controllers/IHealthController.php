79e915544622938e50c299a0696ffa2cf1f48c73
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2024-02-29 10:28:01 +0300

    поправлено реєстрацію по рефералці

--- app/Http/Controllers/IHealthController.php
+++ app/Http/Controllers/IHealthController.php
@@ -24,26 +24,5 @@ class IHealthController extends Controller
         return redirect()->route('register')
             ->withCookie('referrerHash', $data)
             ->withCookie('referrerName', (!empty($User) ? $User['first_name'] . ' ' . $User['last_name'] : ''));
-
-//        $UserParent = User::where('referrer_hash', '=', $data)->first();
-//
-//        $newUser = User::create([
-//            'first_name' => $data['first_name'],
-//            'last_name' => $data['last_name'],
-//            'email' => $data['email'],
-//            'password' => Hash::make($data['password']),
-//            'referrer_hash' => (new RegistrationController())->generationReferrerHash(),
-//            'verification_withdrawal' => 0,
-//            'verification_tariff_closing' => 0,
-//        ]);
-//
-//        if (!empty($UserParent)) {
-//            ReferralsUserSeeder::create([
-//                $newUser->id,
-//                $UserParent->id,
-//            ]);
-//        }
-//
-//        return redirect('/login');
     }
 }
